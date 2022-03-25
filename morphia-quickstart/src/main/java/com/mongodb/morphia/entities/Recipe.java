4f57c0b1fe327f6ecbc8e5f01168a74e9eed9a84
Maxime Beugnet <maxime.beugnet@gmail.com>
2022-03-25 20:50:02 +0100

    rename package

--- morphia-quickstart/src/main/java/com/mongodb/morphia/entities/Recipe.java
+++ morphia-quickstart/src/main/java/com/mongodb/morphia/entities/Recipe.java
@@ -1,49 +1,9 @@
 package com.example.app;
 
-import dev.morphia.annotations.Entity;
-import dev.morphia.annotations.Id;
-
-import dev.morphia.annotations.Field;
-import dev.morphia.annotations.Index;
-import dev.morphia.annotations.IndexOptions;
-import dev.morphia.annotations.Indexes;
-import dev.morphia.annotations.Reference;
-
+import dev.morphia.annotations.*;
 import org.bson.types.ObjectId;
 
 import java.util.List;
-
-/**** Define entities ****/
-
-// Ingredient Model
-@Entity("ingredients")
-@Indexes(@Index(options = @IndexOptions(name = "name"), fields = @Field("name")))
-class Ingredient {
-    @Id
-    private ObjectId id;
-    private String name;
-    private Boolean healthy;
-
-    Ingredient(String name) {
-        this.name = name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setHealthy(Boolean healthy) {
-        this.healthy = healthy;
-    }
-
-    public String toString() {
-        return this.name;
-    }
-}
 
 // Recipe Model
 @Entity("recipes")
